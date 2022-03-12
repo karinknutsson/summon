3774a3aac4c391abb619e1e7689c220ccef72bc0
karin knutsson <karinmiriam@gmail.com>
2022-03-12 15:25:13 +0100

    added UserMixin to User model

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_login import UserMixin
 
 class Event(db.Model):
     __tablename__ = 'events'
@@ -23,7 +23,7 @@ class Event(db.Model):
         return '<id {}>'.format(self.id)
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
